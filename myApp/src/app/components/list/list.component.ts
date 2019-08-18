b9aa183b4f62a8ae6128bd58639567ad35aaa30c
ivanaolortegui <olorteguimor@gmail.com>
2019-08-18 13:42:02 -0500

    ✏ Editando titulo de lista

--- myApp/src/app/components/list/list.component.ts
+++ myApp/src/app/components/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController, IonList } from '@ionic/angular';
 
 import { ToDoService } from '../../services/to-do.service';
 import { List } from '../../models/list-model';
@@ -11,10 +12,11 @@ import { List } from '../../models/list-model';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  @ViewChild(IonList) list: IonList;
 
   @Input() finished = true;
 
-  constructor(public toDo: ToDoService, private router: Router) { }
+  constructor(public toDo: ToDoService, private router: Router, private alertController: AlertController ) { }
 
   ngOnInit() { }
 
@@ -28,6 +30,39 @@ export class ListComponent implements OnInit {
   deleteList(list: List) {
     this.toDo.deleteList(list);
 
+  }
+
+  async editeList(list: List) {
+    const alert = await this.alertController.create({
+      header: 'Nueva lista',
+      inputs: [{
+        name: 'title',
+        type: 'text',
+        value: list.title,
+        placeholder: 'Nombre de la lista'
+      }],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.list.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Actualizar',
+          handler: (data) => {
+            if (data.title.length === 0) {
+              return;
+            }
+            list.title = data.title;
+            this.toDo.saveStorage();
+            this.list.closeSlidingItems();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
